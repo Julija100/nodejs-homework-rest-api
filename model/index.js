6dcac6d2db98c7ec03ab82edf452197d150e9dc6
Julija Faltina <dashinicha@gmail.com>
2021-11-03 19:48:25 +0200

    checkpoint

--- model/index.js
+++ model/index.js
@@ -1,15 +1,43 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require("fs/promises");
+// const contacts = require('./contacts.json');
+const contactsPath = require("./contactsPath");
+const getAllContacts = require("../utils/getAllContacts");
 
-const listContacts = async () => {}
+const listContacts = async () => await getAllContacts(contactsPath);
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const allContacts = await getAllContacts();
 
-const removeContact = async (contactId) => {}
+  const necessaryContact = allContacts.find(
+    ({ id }) => id.toString() === contactId.toString()
+  );
+  if (necessaryContact) {
+    return necessaryContact;
+  } else {
+    return null;
+  }
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  const prevContact = await getAllContacts();
+  const deletedContact = prevContact.find(
+    ({ id }) => id.toString() === contactId.toString()
+  );
 
-const updateContact = async (contactId, body) => {}
+  if (!deletedContact) {
+    return null;
+  }
+  const newContact = prevContact.filter(
+    ({ id }) => id.toString() !== contactId.toString()
+  );
+  await fs.writeFile(contactsPath, JSON.stringify(newContact, null, 2));
+
+  return newContact;
+};
+
+const addContact = async (body) => {};
+
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -17,4 +45,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
