761abe31de81d679e73210de42df8df9b10d67c3
Julija Faltina <dashinicha@gmail.com>
2021-11-08 20:12:04 +0200

    Realise requests/responses

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,90 @@
 const express = require('express')
 const router = express.Router()
+const { NotFound, BadRequest } = require('http-errors')
+
+const contacts = require('../../model/index.js')
+const contactsValidation = require('../../utils/contactsValidation')
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const allContacts = await contacts.listContacts()
+
+    res.json(allContacts)
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params
+    const contact = await contacts.getContactById(contactId)
+
+    if (!contact) {
+      throw new NotFound('Not found')
+    }
+
+    res.status(200).json({ status: 'success', code: 200, data: { contact } })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { error } = contactsValidation.validate(req.body)
+
+    if (error) {
+      throw new BadRequest(error.message)
+    }
+    const contact = await contacts.addContact(req.body)
+
+    res.status(201).json({ status: 'success', code: 201, data: contact })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params
+
+    if (!contactId) {
+      throw new NotFound('Not found')
+    }
+
+    await contacts.removeContact(contactId)
+    res
+      .status(200)
+      .json({ status: 'success', code: 200, message: 'delete contact' })
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.post('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.put('/:contactId', async (req, res, next) => {
+  try {
+    const { error } = contactsValidation.validate(req.body)
+
+    if (error) {
+      throw new BadRequest(error.message)
+    }
+
+    const { contactId } = req.params
+    const updatedContact = await contacts.updateContact(contactId, req.body)
+
+    if (!updatedContact) {
+      throw new NotFound('Not found')
+    }
+
+    res.status(201).json({
+      status: 'success',
+      code: 201,
+      data: updatedContact,
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
