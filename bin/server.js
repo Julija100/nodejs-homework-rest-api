11523dbbce8027b4c2709072c7bf6644f11d68d2
Julija Faltina <dashinicha@gmail.com>
2021-11-15 10:13:00 +0200

    Connected mongoose

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,20 @@
 const app = require('../app')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 3000
+const DB_HOST =
+  'mongodb+srv://JuliaFaltina:t1R5Gd9SLXs4LS1Y@cluster0.luj0b.mongodb.net/db-contacts?retryWrites=true&w=majority'
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: http://localhost:${PORT}`)
-})
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(
+        `Server running. Use our API on port: http://localhost:${PORT}`
+      )
+    })
+  })
+  .catch((err) => {
+    console.log(`Server error. Error message: ${err.message}`)
+    process.exit(1)
+  })
