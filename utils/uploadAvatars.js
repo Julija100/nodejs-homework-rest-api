d115ff68e2000a87e2ab77e8ef8c1196a35d1187
Julija Faltina <dashinicha@gmail.com>
2021-11-29 20:47:15 +0200

    File upload + download

--- utils/uploadAvatars.js
+++ utils/uploadAvatars.js
@@ -1,15 +1,16 @@
 const multer = require('multer')
 const path = require('path')
-
-const tempDir = path.join(__dirname, 'temp')
-console.log(tempDir)
+const { v4 } = require('uuid')
+const tempDir = path.join(__dirname, '../temp')
 
 const uploadConfig = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, tempDir)
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname)
+    const extension = path.extname(file.originalname)
+    const filename = `${v4()}${extension}`
+    cb(null, filename)
   },
   limits: {
     fileSize: 2048
